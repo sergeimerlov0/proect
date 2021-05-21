a67901fac8305fd4222815b010a725c140a5dd79
Sergey <sergeymerlov0@gmail.com>
2021-05-21 18:24:20 +0300

    six

--- src/js/script.js
+++ src/js/script.js
@@ -2,19 +2,41 @@ $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 1200,
         adaptiveHeight: false,
+        dotsClass: 'slick-dots',
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    
                     dots: true,
                     arrows: false,
-                    
+                
                     
             }
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      $('.catalog-item__link').each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+      })
+
+      $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    })
   });
